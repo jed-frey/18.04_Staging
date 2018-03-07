31de555c36005c23a6bec9ebbd43f6c162b371a2
Jed <jed@m6700.local>
2018-03-06 22:53:03 -0500

    Added xbindkeys

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -570,3 +570,6 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 awful.util.spawn("nm-applet &")
 -- Disable tap to click.
 awful.util.spawn("synclient MaxTapTime=0")
+-- Bind keys
+awful.util.spawn("xbindkeys")
+
